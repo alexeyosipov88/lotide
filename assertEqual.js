b5a94ce12dcb141aa2bba487ce42aa75b22d3cc0
alexeyosipov88 <aoosipov88@gmail.com>
2021-05-25 08:25:56 +0000

    template literals added

--- assertEqual.js
+++ assertEqual.js
@@ -1,7 +1,7 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected)
-    console.log(' ✅️✅️✅️ ' + 'Assertion Passed: ' + actual + ' === ' + expected);
-  else console.log("🛑️🛑️🛑️" + 'Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(` ✅️✅️✅️ Assertion Passed: ${actual} === ${expected}`);
+  else console.log(`🛑️🛑️🛑️ Assertion Failed: ${actual} !== ${expected}`);
 };
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
