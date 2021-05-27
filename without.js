f06f00548888e7fa788dc469b418f98b044865fb
alexeyosipov88 <aoosipov88@gmail.com>
2021-05-27 09:56:10 +0000

    added without

--- without.js
+++ without.js
@@ -19,7 +19,7 @@ const without = function(source, itemsToRemove) {
   for (let i = 0; i < source.length; i++) {
     for (let j = 0; j < itemsToRemove.length; j++) {
       if (source[i] === itemsToRemove[j]) {
-        newArray.push(i);
+        newArray.push(i)
         break;
       }
     }
