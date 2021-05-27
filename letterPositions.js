fc798ee37f4228065f3b56510d31b71b0a2092c5
alexeyosipov88 <aoosipov88@gmail.com>
2021-05-27 17:23:38 +0000

    letter positions added

---
+++ letterPositions.js
@@ -0,0 +1,17 @@
+
+const letterPositions = function(string) {
+  const results = {};
+  for (letter in string) {
+    console.log(string[letter])
+    if (results[string[letter]]) {
+      results[string[letter]].push(letter);
+    } else if (string[letter] === " ") {
+      ;
+    } else {
+      results[string[letter]] = [];
+      results[string[letter]].push(letter);
+    }
+  }
+  return results;
+}
+console.log(letterPositions("lighthouse in the house"))
