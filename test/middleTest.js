6af6599a6527a6e8d1a2ec6703a77268436bc326
alexeyosipov88 <aoosipov88@gmail.com>
2021-06-01 12:01:09 +0000

    applied mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,15 @@
 const assertArraysEqual = require("../assertArraysEqual")
 const middle = require("../middle")
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.equal(middle([1, 2, 3]), [2]);
+  });
+})
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
